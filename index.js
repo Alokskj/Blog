b62e94c644a4cb2202a2668f38e961b0be363250
alokskj <alokskj14@gmail.com>
2023-02-12 20:39:00 +0530

    version 2.0

--- index.js
+++ index.js
@@ -4,17 +4,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const posts =[{
-  title: "HOME",
-  content:"Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
-}];
+const mongoose = require("mongoose");
+const blog = require("./src/work.js")
+// const posts =[{
+//   title: "HOME",
+//   content:"Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
+// }];
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017/Blog')
+.then( console.log("connected to mongodb"))
+.catch(error => handleError(error));
 
 
+let posts;
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+
+
 
 const app = express();
 
@@ -24,7 +34,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-app.get("/", (req ,res)=>{
+
+
+app.get("/", async (req ,res)=>{
+     
+  posts = await blog.find({});
+
+   
+
+
   res.render("home", (
     {
       homeStartingContent: homeStartingContent,
@@ -54,11 +72,15 @@ app.get("/compose", (req ,res)=>{
   ))
 });
 app.get('/posts/:newPost', (req ,res )=>{
+
+  
+
+
   let requestedTitle = req.params.newPost;
   posts.forEach((post)=>{
   let storedTitle = post.title;
   if(storedTitle === requestedTitle){
-    console.log("match found!")
+    
     res.render("post", (
       {
         
@@ -66,41 +88,27 @@ app.get('/posts/:newPost', (req ,res )=>{
         postContent:post.content,
       }
     ))
-  }else{
-    console.log("Not found")
   }
   
 });
 })
 
 
-app.post("/compose", (req, res)=>{
+app.post("/compose", async (req, res)=>{
   
-  let post ={
+  const newBlog = {
     title: req.body.postTitle,
     content: req.body.postContent,
+    image : req.body.imageurl,
   }
 
-  posts.push(post);
+  const result = await blog.insertMany([newBlog]);
+  
+ 
   res.redirect("/");
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
---
+++ src/work.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+
+const blogSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required:true,
+        
+
+    },
+    content:{
+        type: String,
+        required: true,
+
+    },
+    image:{
+        type:String,
+        required: true
+    },
+
+})
+
+const blog = mongoose.model("blogs", blogSchema)
+
+module.exports = blog;
+
+
+ 
