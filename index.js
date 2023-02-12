b0f26d652dbd3f6b97f5354418cecd9c8ac181ea
alokskj <alokskj14@gmail.com>
2023-02-12 23:04:07 +0530

    test

--- index.js
+++ index.js
@@ -12,50 +12,50 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.set("strictQuery", false);
-mongoose
-  .connect(
-    "mongodb+srv://alokskj:9814204411j@todocluster.u1jdnxy.mongodb.net/?retryWrites=true&w=majority/blog"
-  )
-  .then(console.log("connected to mongodb"))
+// mongoose.set("strictQuery", false);
+// mongoose
+//   .connect(
+//     "mongodb+srv://alokskj:9814204411j@todocluster.u1jdnxy.mongodb.net/?retryWrites=true&w=majority/blog"
+//   )
+//   .then(console.log("connected to mongodb"))
   
 
 app.get("/", async (req, res) => {
-  posts = await blog.find({});
-
-  res.render("home", {
-    posts: posts,
-  });
+  // posts = await blog.find({});
+  // res.render("home", {
+  //   posts: posts,
+  // });
+  res.send("hello world")
 });
 
-app.get("/compose", (req, res) => {
-  res.render("compose", {});
-});
+// app.get("/compose", (req, res) => {
+//   res.render("compose", {});
+// });
 
-app.get("/posts/:newPost", (req, res) => {
-  let requestedTitle = req.params.newPost;
-  posts.forEach((post) => {
-    let storedTitle = post.title;
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        postTitle: post.title,
-        postContent: post.content,
-      });
-    }
-  });
-});
+// app.get("/posts/:newPost", (req, res) => {
+//   let requestedTitle = req.params.newPost;
+//   posts.forEach((post) => {
+//     let storedTitle = post.title;
+//     if (storedTitle === requestedTitle) {
+//       res.render("post", {
+//         postTitle: post.title,
+//         postContent: post.content,
+//       });
+//     }
+//   });
+// });
 
-app.post("/compose", async (req, res) => {
-  const newBlog = {
-    title: req.body.postTitle,
-    content: req.body.postContent,
-    image: req.body.imageurl,
-  };
+// app.post("/compose", async (req, res) => {
+//   const newBlog = {
+//     title: req.body.postTitle,
+//     content: req.body.postContent,
+//     image: req.body.imageurl,
+//   };
 
-  const result = await blog.insertMany([newBlog]);
+//   const result = await blog.insertMany([newBlog]);
 
-  res.redirect("/");
-});
+//   res.redirect("/");
+// });
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
