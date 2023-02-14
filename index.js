22740943f56277f55ae2fb963d692b668cb64a96
alokskj <alokskj14@gmail.com>
2023-02-14 13:08:29 +0530

    test 1

--- index.js
+++ index.js
@@ -12,25 +12,25 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// mongoose.set("strictQuery", false);
-// mongoose
-//   .connect(
-//     "mongodb+srv://alokskj:9814204411j@todocluster.u1jdnxy.mongodb.net/?retryWrites=true&w=majority/blog"
-//   )
-//   .then(console.log("connected to mongodb"))
-  
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://alokskj:9814204411j@todocluster.u1jdnxy.mongodb.net/?retryWrites=true&w=majority/blog"
+  )
+  .then(console.log("connected to mongodb"));
 
-app.get("/", async (req, res) => {
-  // posts = await blog.find({});
-  // res.render("home", {
-  //   posts: posts,
-  // });
-  res.send("hello world")
+
+app.get("/", (req, res) => {
+ const result = async ()=>{
+  const result = await blog.find({})
+    res.send(result)}
+  result()
+  
 });
 
-// app.get("/compose", (req, res) => {
-//   res.render("compose", {});
-// });
+app.get("/compose", (req, res) => {
+  res.render("compose", {});
+});
 
 // app.get("/posts/:newPost", (req, res) => {
 //   let requestedTitle = req.params.newPost;
