623f07255ed1d850ca616ad3ce3a9e3f858fe3f1
alokskj <alokskj14@gmail.com>
2023-07-09 17:11:07 +0530

    fix errors

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const blog = require("./src/work.js");
 let posts;
 
 const app = express();
-app.set("view engine", "ejs");
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 mongoose.set("strictQuery", false);
 mongoose
   .connect(
-    "mongodb+srv://alokskj:9814204411j@todocluster.u1jdnxy.mongodb.net/?retryWrites=true&w=majority/blog"
+    "mongodb+srv://alokskj:9814204411j@todocluster.u1jdnxy.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(console.log("connected to mongodb"));
 
@@ -23,39 +24,50 @@ mongoose
 app.get("/", (req, res) => {
  const result = async ()=>{
   const result = await blog.find({})
-    res.send(result)}
+    res.render("home", {posts : result})
+ }
   result()
   
 });
 
 app.get("/compose", (req, res) => {
-  res.render("compose", {});
+  res.render("compose");
 });
 
-// app.get("/posts/:newPost", (req, res) => {
-//   let requestedTitle = req.params.newPost;
-//   posts.forEach((post) => {
-//     let storedTitle = post.title;
-//     if (storedTitle === requestedTitle) {
-//       res.render("post", {
-//         postTitle: post.title,
-//         postContent: post.content,
-//       });
-//     }
-//   });
-// });
+app.get("/posts/:id",async (req, res) => {
+  let id = req.params.id;
+  const post = await blog.findById(id)
+  res.render("post", {
+    postTitle: post.title,
+    postContent: post.content,
+    postId : post._id
+  });
+});
 
-// app.post("/compose", async (req, res) => {
-//   const newBlog = {
-//     title: req.body.postTitle,
-//     content: req.body.postContent,
-//     image: req.body.imageurl,
-//   };
 
-//   const result = await blog.insertMany([newBlog]);
+app.post("/compose", async (req, res) => {
+  const newBlog = new blog({
+    title: req.body.postTitle,
+    content: req.body.postContent,
+    image: req.body.imageurl,
+  });
+ 
 
-//   res.redirect("/");
-// });
+  await newBlog.save();
+  res.redirect("/");
+});
+
+app.post("/posts/:id", async (req, res) => {
+  const postId = req.params.id;
+
+  try {
+    await blog.findByIdAndDelete(postId);
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("An error occurred while deleting the post.");
+  }
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
